c256d373ebd780d9bd8cbeb547d6f2ab896df9e7
Igor Dorovskikh <igor@codefitness.us>
2021-02-09 21:12:11 -0500

    recorded first test

--- OrderMEUITests/OrderMEUITests.swift
+++ OrderMEUITests/OrderMEUITests.swift
@@ -11,33 +11,28 @@ import XCTest
 class OrderMEUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        
+        app.staticTexts["Login Later"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.cells.containing(.staticText,
+                                     identifier: "12229 Ventura Blvd, Studio City, CA").element.swipeUp()
+        tablesQuery.staticTexts["Republique"].tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.staticTexts["Detect table"].tap()
+        app.textFields["tableNumberTextField"].tap()
+        app.buttons["Select table"].tap()
+        collectionViewsQuery.staticTexts["Call a waiter"].tap()
+        app.alerts["The waiter is on his way"].scrollViews.otherElements.buttons["Bring a menu"].tap()
+        app.alerts["Got it!"].scrollViews.otherElements.buttons["OK"].tap()
+        
     }
 }
