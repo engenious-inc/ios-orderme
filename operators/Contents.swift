3eaf7ab51c7ac2af3c748e721bfdeff7fb1ae459
JuliaK-118 <ykaranevskaya@engenious.io>
2023-09-05 22:02:01 +0300

    operators

---
+++ operators/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let sum = 15 + 30
+
+let difference = sum - 20
+
+var product = 8 * 7
+
+let quotient = 50 / 2
+
+let quotient2 = 11 / 2
+
+let result = 11 % 2
+
+product *= 2
+
+product = product * 2
