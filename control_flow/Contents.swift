58cd5713ce25c9fd773ac086c76270e9c4730eee
JuliaK-118 <ykaranevskaya@engenious.io>
2023-09-05 22:04:40 +0300

    control_flow

---
+++ control_flow/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+// 👶  👨  👴
+
+
+let age: Int = 25
+
+if age < 18 {
+    print("👶")
+} else {
+    print("👨")
+}
+
+if age < 18 {
+    print("👶")
+} else if age > 100 {
+    print("👴")
+} else {
+    print("👨")
+}
