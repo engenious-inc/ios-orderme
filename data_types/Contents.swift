cfd5e45753e836066d7aba14a9173ea7106b98c6
JuliaK-118 <ykaranevskaya@engenious.io>
2023-09-05 21:58:45 +0300

    data_types

---
+++ data_types/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+var age: Int = 25
+age = age + 1
+age += 1
+
+let fullName: String = "John Smith"
+
+fullName.count
+
+fullName.isEmpty
+
+var flag: Bool = false
+
+let char: Character = "H"
+
+let pi: Double = 3.14159265
