e7aad6b547be235253c047d3a663659e02f68435
Oleksii Kotsevych <>
2024-06-11 13:48:20 +0200

    Added 'Codegen' tests generator. Added 300 UITests.

---
+++ OrderMEUITests/Tests/DemoTests/SampleAppUITest_60.swift
@@ -0,0 +1,20 @@
+
+import XCTest
+
+class SampleAppUITest_60: BaseTest {
+
+    override func setUp() {
+        super.setUp()
+        app.launchTunnel()
+    }
+
+    func testVerifyMyReservationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+
+}
---
+++ Scripts/Codegen.py
@@ -0,0 +1,43 @@
+# Codegen.py
+import os
+import sys
+
+def generate_test_files(num_files, possibility):
+    for i in range(num_files):
+        with open(f"SampleAppUITest_{i}.swift", 'w') as file:
+            file.write(f'''
+import XCTest
+
+class SampleAppUITest_{i}: BaseTest {{
+
+    override func setUp() {{
+        super.setUp()
+        app.launchTunnel()
+    }}
+
+    func testVerifyMyReservationsTabIsRestricted() {{
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }}
+
+}}
+''')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py [number_of_files, possibility]")
+        sys.exit(1)
+    try:
+        num_files = int(sys.argv[1])
+        possibility = int(sys.argv[2])
+    except ValueError:
+        print("Invalid args")
+        sys.exit(1)
+
+    generate_test_files(num_files, possibility)
+    print('All test files generated.')  
