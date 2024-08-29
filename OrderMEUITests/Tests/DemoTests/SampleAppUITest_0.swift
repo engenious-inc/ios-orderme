3ac2c0f80f584ec4d18398cd9498cae17b589efc
Oleksii Kotsevych <>
2024-08-29 16:34:22 +0200

    Added tests.

--- OrderMEUITests/Tests/DemoTests/SampleAppUITest_0.swift
+++ OrderMEUITests/Tests/DemoTests/SampleAppUITest_0.swift
@@ -22,5 +22,54 @@ class SampleAppUITest_0: BaseTest {
         XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
                   "You did not login alert is not present")
     }
+    func testVerifyMyR3eservationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+    func testVerifyMyRes3ervationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+    func testVerifyMyRese2rvationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+    func testVerifyM1yReservationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+    func testVerifyMyReservationsTa5bIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+    func testVerifyMyReservationsTabIsRestri6cted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+
 
 }
