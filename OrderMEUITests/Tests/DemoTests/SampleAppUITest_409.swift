2eb2d36b1c240e45007413053547a57e5d1404bd
Oleksii Kotsevych <>
2024-10-04 10:15:29 +0200

    Removed 300 tests and added 1000 tests.

---
+++ OrderMEUITests/Tests/DemoTests/SampleAppUITest_409.swift
@@ -0,0 +1,24 @@
+
+import XCTest
+
+class SampleAppUITest_409: BaseTest {
+
+    override func setUp() {
+        super.setUp()
+        app.launch()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testVerifyMyReservationsTabIsRestricted() {
+        let loginScreen = LoginScreen()
+        let restaurantsListScreen = loginScreen.loginLater()
+        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+
+        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
+                  "You did not login alert is not present")
+    }
+
+}
