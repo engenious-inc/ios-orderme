76c8810af6682dbb5465a07000563f3aceed3744
Oleksii Kotsevych <>
2024-11-08 13:45:53 +0100

    Added 50 UI tests.

--- OrderMEUITests/Tests/DemoTests/SampleAppUITest_41.swift
+++ OrderMEUITests/Tests/DemoTests/SampleAppUITest_41.swift
@@ -12,13 +12,47 @@ class SampleAppUITest_41: BaseTest {
         super.tearDown()
     }
 
-    func testVerifyMyReservationsTabIsRestricted() {
-        let loginScreen = LoginScreen()
-        let restaurantsListScreen = loginScreen.loginLater()
-        let myReservationsScreen = restaurantsListScreen.tapMyReservations()
+func testBringAMenu() throws {
+    let app = XCUIApplication()
+    app.launch()
 
-        XCTAssert(myReservationsScreen.youDidNotLoginAlert.waitForExistence(timeout: 2),
-                  "You did not login alert is not present")
-    }
+    let loginLaterButton = app.buttons["loginLaterButton"]
+    loginLaterButton.tap()
+
+    let republiqueRest = app.staticTexts["Republique"]
+    republiqueRest.tap()
+
+    let detectTableOption = app.collectionViews.staticTexts["Detect table"]
+    detectTableOption.tap()
+
+    let tableNumberField = app.textFields["tableNumberTextField"]
+    tableNumberField.tap()
+    tableNumberField.typeText("3")
+    let selectTableButton = app.buttons["Select table"]
+    selectTableButton.tap()
+
+    let callAWaiterOption = app.collectionViews.staticTexts["Call a waiter"]
+    callAWaiterOption.tap()
+
+    let waiterAlert = app.alerts["The waiter is on his way"]
+    let bringAMenuButton = waiterAlert.buttons["Bring a menu"]
+    bringAMenuButton.tap()
+
+    let gotItAlert = app.alerts["Got it!"]
+    XCTAssert(gotItAlert.waitForExistence(timeout: 2), "Got it alert is not present")
+
+    callAWaiterOption.tap()
+
+    let bringTheBillButton = waiterAlert.buttons["Bring the bill"]
+    bringTheBillButton.tap()
+
+    XCTAssert(gotItAlert.waitForExistence(timeout: 2), "Got it alert is not present")
+
+    let restaurantScreen = RestaurantScreen()
+    restaurantScreen.choose(option: .callRestaurant)
+
+    XCTAssert(restaurantScreen.callAlert.waitForExistence(timeout: 2),
+         "Call alert is not present")
+  }
 
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# OrderMe - POS Restaurant iOS App 
-It was exclusively built for Engneious University specifically for  XCUITest Course
