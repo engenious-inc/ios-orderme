8b30111189184476f160dd48040b50b6231d0eb4
Daniel Augustynowicz <auhustsinovich.d@gmail.com>
2024-11-26 12:23:06 +0100

    update project

---
+++ OrderMEUITests/Tests/FacebookTests.swift
@@ -0,0 +1,26 @@
+//
+//  FacebookTests.swift
+//  orderMe
+//
+//  Created by Daniil Auhustsinovich on 26/11/2024.
+//  Copyright © 2024 Boris Gurtovoy. All rights reserved.
+//
+
+import XCTest
+
+class FacebookTests: BaseTest {
+    override func tearDown() {
+        Springboard.deleteApp()
+        super.tearDown()
+    }
+
+    func testLoginWithFacebook() {
+        LoginScreen()
+            .loginWithFacebook()
+        FacebookLoginScreen()
+            .typeEmail("zkpedymhza_1614299001@tfbnw.net")
+            .typePassword("orderme12345")
+            .login()
+            .continueWithFacebook()
+    }
+}
--- OrderMEUITests/Utils/Springboard.swift
+++ OrderMEUITests/Utils/Springboard.swift
@@ -10,4 +10,14 @@ import XCTest
 
 enum Springboard {
     static let shared: XCUIApplication = .init(bundleIdentifier: "com.apple.springboard")
+
+    static func deleteApp() {
+        guard shared.icons["OrderMe"].exists else {
+            return
+        }
+        shared.icons["OrderMe"].firstMatch.press(forDuration: 2.0)
+        shared.buttons["Remove App"].firstMatch.tap()
+        shared.buttons["Delete App"].firstMatch.tap()
+        shared.buttons["Delete"].firstMatch.tap()
+    }
 }
