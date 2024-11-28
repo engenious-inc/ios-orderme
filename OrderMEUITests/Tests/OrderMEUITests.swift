0cd51658616adf06906ebfc83eddbd276bb1b284
Daniel Augustynowicz <auhustsinovich.d@gmail.com>
2024-11-28 12:17:29 +0100

    move test to another folder.

--- OrderMEUITests/Tests/OrderMEUITests.swift
+++ OrderMEUITests/Tests/OrderMEUITests.swift
@@ -77,43 +77,4 @@ class OrderMEUITests: BaseTest {
             FacebookLoginScreen().continueWithFacebook()
         }
     }
-
-    func testChallenge() {
-        let today = Date()
-        guard let futureDate = today.getUIDateForTodayPlus(days: 3) else {
-            XCTFail("Unable to create a future date")
-            return
-        }
-        let day = futureDate.day
-        let month = futureDate.month
-
-        LoginScreen()
-            .loginWithFacebook()
-        if !FacebookLoginScreen().isContinueWithFacebookButtonVisible() {
-            FacebookLoginScreen()
-            .typeEmail("zkpedymhza_1614299001@tfbnw.net")
-            .typePassword("orderme12345")
-            .login()
-            .continueWithFacebook()
-        } else {
-            FacebookLoginScreen().continueWithFacebook()
-        }
-        
-        RestaurantsListScreen()
-            .openHakkasanRestaurant()
-        RestaurantScreen()
-            .choose(option: .reservation)
-        ReservationsScreen()
-            .assertBookButtonIsPresent()
-            .typePhoneNumber("2124567890")
-            .typeNumberOfPeople("2")
-            .selectDate(month: month, day: day, hour: 7, minute: 45, amPm: .pmTime)
-            .reserve()
-            .assertSuccessAllertExist()
-            .tapOK()
-            .tapMyReservations()
-        MyReservationsScreen()
-            .assertCurrentReservationsButtonIsSelected()
-            .assertReservationInHakkasan()
-    }
 }
